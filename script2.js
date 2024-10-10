d95e980375c8008c2b918396af886cb422bc923a
Tribal Developer <125229126+tribal-developer@users.noreply.github.com>
2024-10-09 19:28:32 -0700

    some changes.

--- script2.js
+++ script2.js
@@ -1,11 +1,16 @@
-const ul = document.querySelector('ul');
-function hide() {
-    ul.style.display = 'none'
-}
-function show() {
-    if(ul.style.display === "flex"){
-        ul.style.display = 'flex'
-    }else{
-        alert('Switch to Desktop Mode..!')
-    }
-}
+
+
+const ul = document.querySelector('.ul');
+const show = document.querySelector('.show-btn');
+const hide = document.querySelector('.toggle-btn');
+
+show.addEventListener('click', (e)=>{
+    ul.style.display = 'flex';
+    ul.style.top = '0';
+    ul.style.transition = 'all .5s'
+})
+hide.addEventListener('click', (e)=>{
+    ul.style.display = 'none';
+    ul.style.top = '-16rem';
+     ul.style.transition = 'all 6s'
+})
